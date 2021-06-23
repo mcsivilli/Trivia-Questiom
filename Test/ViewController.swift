b6034d9d840a49b2b1bc85d98caf9ad9db49eac7
Maddlyn Sivilli <mcsivilli@gmail.com>
2021-06-23 12:22:36 -0400

    Iteration 0

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var triviaQuestion: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func correctAnswer(_ sender: UIButton) {
+        if let response = textField.text {
+             = response
+        }
+    }
 }
+    
+
 
